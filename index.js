f73bab5cd88c0f7c6b4ae7a102c061869d6e9a85
Do Thanh <dvthanh277@aiaracorp.com>
2022-12-10 16:02:52 +0700

    Init

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const { ZingMp3 } = require("zingmp3-api-full")
 const app = express()
+const cors = require('cors')
 
+app.use(cors({
+    origin: "*"
+}))
 app.get('/', (req, res) => res.send('Hello '))
 
 app.get('/api/home', (req, res) => {
