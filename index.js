ac5bf9098345fb5dc645add6ff0f0e9a68ae614e
Do Thanh <dvthanh277@aiaracorp.com>
2022-12-11 22:35:47 +0700

    Update api get detail playlist bottom

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require('express')
-const { ZingMp3 } = require("zingmp3-api-full-v2")
 const app = express()
 const cors = require('cors')
+const { ZingMp3 } = require('./zingmp3')
 
 app.use(cors({
     origin: "*"
 }))
-app.get('/', (req, res) => res.send('Hello '))
+app.get('/', (req, res) => res.send('Hello Ooms'))
 
-app.get('/api/home', (req, res) => {
+app.get('/ooms-api/home', (req, res) => {
     ZingMp3.getHome().then((data) => {
         return res.send(data)
     })
 })
-app.get('/api/detailplaylist', (req, res) => {
+app.get('/ooms-api/detailplaylist', (req, res) => {
     const id = req.query.id
     ZingMp3.getDetailPlaylist(id).then((data) => {
         return res.send(data)
     })
 })
-app.get('/api/song', (req, res) => {
+app.get('/ooms-api/detaibottom', (req, res) => {
+    const id = req.query.id
+    ZingMp3.getDetailPlaylistBottom(id).then((data) => {
+        return res.send(data)
+    })
+})
+app.get('/ooms-api/song', (req, res) => {
     const id = req.query.id
     ZingMp3.getSong(id).then((data) => {
+        return res.send(data)
+    })
+})
+app.get('/ooms-api/artist', (req, res) => {
+    const name = req.query.name
+    ZingMp3.getArtist(name).then((data) => {
         return res.send(data)
     })
 })
