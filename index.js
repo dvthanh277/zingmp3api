0204f9dc3ed618d047368aac5b3f6b356136d760
Do Thanh <dvthanh277@aiaracorp.com>
2022-12-11 16:27:14 +0700

    DetailPlaylist

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { ZingMp3 } = require("zingmp3-api-full")
+const { ZingMp3 } = require("zingmp3-api-full-v2")
 const app = express()
 const cors = require('cors')
 
@@ -12,8 +12,21 @@ app.get('/api/home', (req, res) => {
     ZingMp3.getHome().then((data) => {
         return res.send(data)
     })
-
 })
+app.get('/api/detailplaylist', (req, res) => {
+    const id = req.query.id
+    ZingMp3.getDetailPlaylist(id).then((data) => {
+        return res.send(data)
+    })
+})
+app.get('/api/song', (req, res) => {
+    const id = req.query.id
+    ZingMp3.getSong(id).then((data) => {
+        return res.send(data)
+    })
+})
+// "ZWDF7UEF"
+
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
